a338f006217f9355248861660c39129068e46935
Alsou <62538555+Alsou139@users.noreply.github.com>
2020-10-19 20:14:51 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-
 const link = document.querySelectorAll('.item-body-list-item');
 
 for (let i = 0; i < link.length; i++) {
 	link[i].addEventListener('click', function () {
-		link.forEach(c => {
-			c.classList.remove('active');
-		})
+		for (let j = 0; j < link.length; j++)
+			link[j].classList.remove('active');
 		this.classList.add('active');
 	})
 }
